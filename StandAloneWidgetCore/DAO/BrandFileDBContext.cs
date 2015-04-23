24dcc367abc9fcf38ecd9e854183dba0b0ab41e9
eddherrera-4th <eduardo.herrera@4thsource.com>
2015-04-23 08:58:03 -0500

    Add Solution, core  and testing

---
+++ StandAloneWidgetCore/DAO/BrandFileDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SolutionZ.StandAloneWidget.BO;
+
+namespace SolutionZ.StandAloneWidget.DAO
+{
+    public partial class StandAloneWidgetContext : DbContext
+    {
+        /// <summary>
+        /// BrandFiles DBContext
+        /// </summary>
+        public DbSet<BrandFile> BrandFiles { get; set; }
+
+    }
+}
