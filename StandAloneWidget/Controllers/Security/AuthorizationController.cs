9853e3448d42a9ea3f399d07cf4976a37e63aace
eddherrera-4th <eduardo.herrera@4thsource.com>
2015-04-30 17:24:52 -0500

    Login UI Implementation

--- StandAloneWidget/Controllers/Security/AuthorizationController.cs
+++ StandAloneWidget/Controllers/Security/AuthorizationController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using StandAloneWidget.Models.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,10 +13,56 @@ namespace StandAloneWidget.Controllers
     {
         //
         // GET: /Auth/
-
-        public ActionResult LogIn()
+        public ActionResult LogIn(string returnUrl)
         {
-            return View();
+            return View(new LogInModel() { ReturnUrl = returnUrl } );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="credentials"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult LogIn(LogInModel credentials)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            //TODO: User sign, should be something like credentials valid or something! Add OUT parameter to tell what failed make use of the new ASP.NET Identity UserManager to authenticate
+            if (true)
+            {
+                var identity = new ClaimsIdentity(new[] { 
+                    new Claim( ClaimTypes.Name , "Me"),
+                    new Claim( ClaimTypes.Country, "MX") },
+                        "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authorizationManager = ctx.Authentication;
+
+                authorizationManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(credentials.ReturnUrl));
+
+            }
+            else
+            {
+                // user authN failed
+                //TODO: Handle Sighn in Errors
+                //ModelState.AddModelError("", "Invalid email or password");
+                //return View();
+            }
+            
+
+        }
+
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+                return Url.Action("Index", "Admin");
+            else
+                return returnUrl;
         }
 
     }
