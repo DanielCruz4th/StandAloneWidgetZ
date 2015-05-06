90c52dd2ffda9a177e04e1184525d685de90eba2
eddherrera-4th <eduardo.herrera@4thsource.com>
2015-05-06 15:37:59 -0500

    Fix Razor.Engine assembly install and Upload example

---
+++ StandAloneWidgetTesting/UploadedFileTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rainbow.Web.Storage;
+using System.IO;
+
+namespace StandAloneWidgetTesting
+{
+    [TestClass]
+    public class UploadedFileTest
+    {
+        [TestMethod]
+        public void UploadedFile_Insert()
+        {
+            //Arrange
+            string path = @"c:\Koala.jpg";
+            File.Create(path);
+
+
+            UploadedFile newUploadedFile = new UploadedFile();
+            newUploadedFile.InputStream = File.OpenRead(path);
+            newUploadedFile.FileName = "koala";
+
+            
+            //Act
+            newUploadedFile.Save();
+
+        }
+    }
+}
