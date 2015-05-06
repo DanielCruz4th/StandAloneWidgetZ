f5fd2db4bad9c1cdde3764d7698a2e7475313185
eddherrera-4th <eduardo.herrera@4thsource.com>
2015-05-06 11:24:46 -0500

    Template Draft

---
+++ StandAloneWidget/Models/Design/StyleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StandAloneWidget.Models.Design
+{
+    public class StyleModel
+    {
+        public string HeaderBackground { get; set;  }
+
+    }
+}
